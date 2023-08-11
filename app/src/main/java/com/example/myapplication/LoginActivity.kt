fe45d74635ccceb2c1b33fb1238ea4e5af175e3f
Tiago <tiagolachman@gmail.com>
2023-08-11 20:13:13 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        var button = findViewById<Button>(R.id.btnEntrar)
+        button.setOnClickListener {
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/TelaInicialActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class TelaInicialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_inicial)
+//        var button = findViewById<Button>(R.id.btnEntrar)
+//        button.setOnClickListener {
+//            var intent = Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
+//        }
+
+    }
+}
